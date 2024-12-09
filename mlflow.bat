68f91bf2ce3b2f9ad09660535e669267b21c1150
Ismael Miranda <ismaelmiranda11@hotmail.com>
2024-12-09 14:15:45 +0000

    dividing the project into notebooks

--- mlflow.bat
+++
@@ -1,5 +0,0 @@
-@REM Iniciar o ambiente virtual
-@REM call ..\..\.venv\Scripts\activate.bat
-
-@REM Iniciar o servidor MLFlow
-mlflow server --port 8000
---
+++ mlflow_start.bat
@@ -0,0 +1,5 @@
+
+@REM Run this in the root of the project
+mlflow server --port 8000
+
+@REM Access the MLflow UI by navigating to http://localhost:8000 in your web browser.
